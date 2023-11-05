5e6b514a53813e7c582eb5375ae77644af0a0f35
Abdussalam Mujeeb-ur-rahman <103681250+Abdussalam-Mujeeb-ur-rahman@users.noreply.github.com>
2023-11-05 16:01:16 -0500

    Revert ".."

--- src/utils/extractUser.ts
+++ src/utils/extractUser.ts
@@ -5,34 +5,29 @@ const cache = new NodeCache({ stdTTL: 180 });
 
 // Function to extract user data
 // Function to extract user data
-export const extractUserData = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    // Extract the unique ID from the request parameters
-    const { id } = req.params;
-    console.log(id);
-    // Retrieve and delete the random string from the cache using the unique ID
-    let randomString = cache.take(id) as string;
-    console.log(randomString);
-    // If the random string is not found in the cache, send an error message
-    if (!randomString) {
-      return null; // Return null if user data extraction fails
+export const extractUserData = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // Extract the unique ID from the request parameters
+      const { id } = req.params;
+      // Retrieve and delete the random string from the cache using the unique ID
+      let randomString = cache.take(id) as string;
+  
+      // If the random string is not found in the cache, send an error message
+      if (!randomString) {
+        return null; // Return null if user data extraction fails
+      }
+  
+      // Decode the base64-encoded random string back to a JSON string
+      const requestBody = Buffer.from(randomString, "base64").toString();
+      // Parse the JSON string to an object
+      const reqBody = JSON.parse(requestBody);
+      // Extract the name, email, and password from the parsed object
+      const { name, email, password } = reqBody;
+  
+      return { name, email, password };
+    } catch (error) {
+      // Pass the error to the next middleware function
+      next(error);
     }
-
-    // Decode the base64-encoded random string back to a JSON string
-    const requestBody = Buffer.from(randomString, "base64").toString();
-    // Parse the JSON string to an object
-    const reqBody = JSON.parse(requestBody);
-    console.log(reqBody);
-    // Extract the name, email, and password from the parsed object
-    const { name, email, password } = reqBody;
-
-    return { name, email, password };
-  } catch (error) {
-    // Pass the error to the next middleware function
-    next(error);
-  }
-};
+  };
+  
